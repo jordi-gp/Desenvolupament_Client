39e48f0f6a9b80cb86b48a041620a31798e841c9
Jordi Garcia <jordigar00@gmail.com>
2021-10-15 13:52:09 +0200

    e

--- Tema_4/exercici_4_5.js
+++ Tema_4/exercici_4_5.js
@@ -11,16 +11,16 @@ function afegirElement(){
     if(respuesta == ""){
         alert("No pots deixar aquest camp en blanc");
     } else {
-        var objecte = document.getElementsByTagName("ol");   //obtenim el id del llistat amb les etiquetes '<ol></ol>'
-        objecte = document.createElement("li");   //creem l'element de tipus '<li></li>'
-        var contingut = document.createTextNode(respuesta);   //creem el node de text amb el contingut de la variable 'resposta'
-        objecte.appendChild(contingut);   //afegim el node
-        document.body.appendChild(objecte);   //indiquem que 'objecte' es un fill de 'body'
+        var llistat = document.getElementById("listado");
+        var items = document.createElement("li");
+
+        items.appendChild(document.createTextNode(respuesta));
+
+        llistat.appendChild(items);
     }
 }
 
 function borrarElement(){
     var objecte2 = document.getElementsByTagName("li");
-    console.log(objecte2);
     
 }
