bd617bffb2de406b2a0124fdaba68520064a2faf
Jordi García Pons <83701435+jordi-gp@users.noreply.github.com>
2021-10-17 18:50:27 +0200

    Exercici DOM

--- Tema_4/mostrar_ocultar_text/funcions.js
+++ Tema_4/mostrar_ocultar_text/funcions.js
@@ -7,21 +7,46 @@ function main(){
 
 //Mostrar els dos paràgrafs
 function mostrarParagraf(){
+    //Obtenim els elements amb les classes corresponents aplicades
+    var parag1 = document.getElementsByClassName("paragraf1");
+    var parag2 = document.getElementsByClassName("paragraf2");
+
+    var b =[];
+
+    //Obtenim la propietat 'display'
+    var a = window.getComputedStyle(parag1[parag1.length -1], null).getPropertyValue("display");
+    var b = window.getComputedStyle(parag2[parag2.length -1], null).getPropertyValue("display");
+    
+    //Realitzem una comparació, si el paragraf s'esta mostrant,
+    //avisem a l'usuari de que ja està visible, en cas contrari l'ocultem
+    if(a != "block"){
+        parag1 = parag1[parag1.length -1];
+        parag2 = parag2[parag2.length -1];
+
+        parag1.style.display = "block";
+        parag2.style.display = "block";
+    } else {
+        alert("El paràgraf ja esta siguent mostrat");
+    }
+}
+
+//Ocultar els dos paràgrafs
+function ocultarParagraf(){
+    //Exactament el mateix codi que en la funció de dalt
+    //pero en aquest cas serveix per ocultar el paràgraf en cas de no estar-ho
     var parag1 = document.getElementsByClassName("paragraf1");
     var parag2 = document.getElementsByClassName("paragraf2");
 
     var a = window.getComputedStyle(parag1[parag1.length -1], null).getPropertyValue("display");
     var b = window.getComputedStyle(parag2[parag2.length -1], null).getPropertyValue("display");
     
-    if(a != "block"){
-        console.log("no es display: block!");
+    if(a != "none"){
+        parag1 = parag1[parag1.length -1];
+        parag2 = parag2[parag2.length -1];
+
+        parag1.style.display = "none";
+        parag2.style.display = "none";
     } else {
-        console.log("yo me llamo ralph");
+        alert("El paràgraf ja esta ocult");
     }
-
-}
-
-//Ocultar els dos paràgrafs
-function ocultarParagraf(){
-
 }
