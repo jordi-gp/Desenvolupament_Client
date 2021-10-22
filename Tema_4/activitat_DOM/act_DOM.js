1b8c2c2bed5888f806028c736596cff2a2a22bd9
Jordi Garcia <jordigar00@gmail.com>
2021-10-22 13:53:55 +0200

    dom

--- Tema_4/activitat_DOM/act_DOM.js
+++ Tema_4/activitat_DOM/act_DOM.js
@@ -1,11 +1,12 @@
 window.onload = main;
 
 function main(){
-    document.getElementById("crearAdresa").addEventListener("click", afegirElement);
+    document.getElementById("crearAdresa").addEventListener("click", creaElement);
+    carregarElements();
 }
 
 //Funció per poder afegir adreses noves
-function afegirElement(){
+function creaElement(){
 
     //Valors que se li aplicaran a l'objecte
     var nomAdr = document.getElementById("nomAdresa");
@@ -33,26 +34,39 @@ function afegirElement(){
         
         arrElements.push(adresa);
         localStorage.setItem("Elements", JSON.stringify(arrElements));
-        mostrarElement();
+        afegirElement(adresa);
     }
 }
 
-//Funció per mostrar les tasques
-function mostrarElement(){
+//Funció per afegir un element nou
+function afegirElement(elem){
+    var ul = document.getElementById("llista");
+
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    var cont = document.createTextNode(elem.aNom);
+    var url = document.createTextNode(elem.aUrl);
+
+    a.setAttribute("href", url);
+    li.appendChild(a);
+    li.appendChild(cont);
+    li.setAttribute("href", url);
+    li.setAttribute("type", "checkbox");
+    ul.appendChild(li);
+
+    console.log(li.hasAttribute("href"));
+    console.log(a);
+}
+
+//Funció per poder carregar tots els elements
+function carregarElements(){
     var arrElements = [];
 
     if(JSON.parse(localStorage.getItem("Elements")) != null){
         arrElements = JSON.parse(localStorage.getItem("Elements"));
     }
 
-    var list = document.getElementById("llista");
-
     arrElements.forEach(element => {
-        var li = document.createElement("li");
-        var cont = document.createTextNode(element.aNom);
-        li.appendChild(cont);
-        document.body.appendChild(li);
-        console.log(element);
+        afegirElement(element);
     });
-
 }
