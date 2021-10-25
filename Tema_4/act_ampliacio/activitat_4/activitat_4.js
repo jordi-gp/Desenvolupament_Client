8f5b1ec100dc7eed996cf99e8a58a9d684bae2f6
Jordi García Pons <83701435+jordi-gp@users.noreply.github.com>
2021-10-26 00:25:50 +0200

    activitat ampliacio

--- Tema_4/act_ampliacio/activitat_4/activitat_4.js
+++ Tema_4/act_ampliacio/activitat_4/activitat_4.js
@@ -7,17 +7,18 @@ function main(){
 function busca(){
     var par = prompt("Quina paraula desitges buscar");
 
-    var a =document.getElementById("p");
+    var a = document.getElementById("p");
+    var index = a.textContent.indexOf(par);
 
-    var index = a.textContent.includes(par);
-
-    if(index === true){
-        for(var i=0; i < a.textContent.length; i++){
+    if(index >= 0){
+        alert("La paraula " +par+ " es troba al text");
+        
+        /*console.log(index);
+        for(var i=0; i <= index; i++){
+            console.log(a.textContent[i]);
             
-        }
+        }*/
     } else {
         alert("No s'ha trobat la paraula");
     }
-    
-    
 }
