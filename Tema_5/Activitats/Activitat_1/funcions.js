5c9e7b11807dc3d5a8c6ba90c2a309a3efbbd6d6
Jordi Garcia <jordigar00@gmail.com>
2021-10-27 13:51:03 +0200

    Tema 5

---
+++ Tema_5/Activitats/Activitat_1/funcions.js
@@ -0,0 +1,75 @@
+window.onload = main;
+
+var comunitats = [{"comunitat":"Andalucia",
+        "provincies":[
+            "Almería",
+            "Cádiz",
+            "Córdoba",
+            "Granada",
+            "Jaén",
+            "Huelva",
+            "Málaga",
+            "Sevilla"
+    ]},
+    {"comunitat":"Aragón",
+        "provincies":[
+            "Huesca",
+            "Teruel",
+            "Zaragoza"
+    ]},
+    {"comunitat":"Canarias",
+        "provincies":[
+            "Las Palmas",
+            "Santa Cruz de Tenerife"
+    ]},
+    {"comunitat":"Cantabria",
+        "provincies":[
+            "Cantabria"
+    ]},
+    {"comunitat":"Castilla y León",
+        "provincies":[
+            "Ávila",
+            "Burgos",
+            "León",
+            "Palencia",
+            "Salamanca",
+            "Segovia",
+            "Soria",
+            "Toledo",
+            "Zamora"
+    ]},
+    {"comunitat":"Castilla-La Mancha",
+        "provincies":[
+            "Albacete",
+            "Ciudad Real",
+            "Cuenca",
+            "Guadalajara",
+            "Valladolid"
+    ]},
+    {"comunitat":"Cataluña",
+        "provincies":[
+            "Barcelona",
+            "Lleida",
+            "Girona",
+            "Tarragona"
+    ]},
+    {"comunitat":"Ceuta",
+        "provincies":[
+            "Ceuta"
+    ]},
+    {"comunitat":"Comunidad Valenciana",
+        "provincies":[
+            "Alicante",
+            "Castellón",
+            "Valencia"
+    ]},
+    {
+
+    }];
+function main() {
+
+}
+
+function mostrarComunitats() {
+
+}
