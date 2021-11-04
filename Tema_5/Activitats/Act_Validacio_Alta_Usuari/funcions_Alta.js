434f7b374b97b70b50f97b6fdcd42f044568be13
Jordi Garcia <jordigar00@gmail.com>
2021-11-04 11:37:12 +0100

    Actualitzacio activitats validacio

--- Tema_5/Activitats/Act_Validacio_Alta_Usuari/funcions_Alta.js
+++ Tema_5/Activitats/Act_Validacio_Alta_Usuari/funcions_Alta.js
@@ -1,6 +1,49 @@
 window.onload = main;
 
 function main(){
-
+    document.getElementById("Enviar").addEventListener("click", validar, false);
 }
 
+function validar(e){
+    esborraError();
+
+    if(validarNom() && confirm("Confirma si vols enviar el formulari")){
+        return true;
+    } else {
+        e.preventDefault();
+        return false;
+    }
+}
+
+function esborraError(){
+    var formulari = document.forms[0];
+
+    for(var i=0; i < formulari.elements.length; i++){
+        formulari.elements[i].className = "";
+    }
+
+    document.getElementById("missatgeError").innerHTML = "";
+}
+
+function error2(element, missatge){
+    document.getElementById("missatgeError").innerHTML = missatge;
+    element.className = "error";
+    element.focus();
+}
+
+
+function validarNom(){
+    var nom = document.getElementById("nom");
+
+    if(!nom.checkValidity()){
+        if(nom.validity.valueMissing){
+            error2(nom, "Has d'introduïr un nom!");
+        }
+
+        if(nom.validity.patternMismatch){
+            error2(nom, "El nom ha de tindre entre 2 i 14 caracters");
+        }
+        return false;
+    }
+    return true;
+}
