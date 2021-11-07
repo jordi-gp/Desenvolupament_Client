3b50fef12cd265cd39ac811dcb55b0d69e3753c6
Jordi García Pons <83701435+jordi-gp@users.noreply.github.com>
2021-11-07 22:16:38 +0100

    a

--- Tema_5/Activitats/ExtraForm/js/action.js
+++ Tema_5/Activitats/ExtraForm/js/action.js
@@ -1,9 +1,54 @@
-// la imatge de la esquerra ha de canviar quna canvia el desplegable. 
+window.onload = main;
 
-//s'hha de validar el número de serie per a que el numero cumplisca 
-// les seguent regles : 3 numeros inicials, 4 lletres en majúscules
-// i acabar amb el níumero 1 o 2 , o amb la lletra A
+function main(){
+    assignaId();
+    document.getElementById("tipo").addEventListener("change", canviaImatge2);
+    document.getElementById("mostrarDescripcio").addEventListener("click", mostraDescripcio);
+}
 
-//  Al clickar en mostrar Descripció apareixà el text area per poder excriure les dades.
-// cada vegada que s'escriga una paraula al text area s'ha de contar el númerode paraules.
- 
+const valNumSerie = new RegExp(/^\d{3}[A-Z]{4}([1-2]{1}|[A]{1})$/)
+
+//Assignació de id's per a les opcions
+function assignaId(){
+    var options = document.getElementsByTagName("option");
+
+    for(var i=0; i < options.length; i++){
+        options[i].setAttribute("id", i);
+    }
+}
+
+//FORMA DE CANVIAR L'IMATGE SELECCIONADA 1
+/*function canviaImatge(){
+    var lista = document.getElementById("tipo");
+    var idElem = lista[lista.selectedIndex].id;
+
+    var foto = document.getElementById("imagen");
+
+    if(idElem == 0){
+        foto.setAttribute("src", "img/distribucio.jpg");
+    } else if(idElem == 1){
+        foto.setAttribute("src", "img/oficina.jpg");
+    } else if(idElem == 2){
+        foto.setAttribute("src", "img/producion.jpg");
+    }
+}*/
+
+//FORMA DE CANVIAR L'IMATGE SELECCIONADA 2 (més óptima pense yo)
+function canviaImatge2(){
+    var lista = document.getElementById("tipo");
+    var nomElem = lista[lista.selectedIndex].value;
+
+    var foto = document.getElementById("imagen");
+    var rutaImg = "img/"+nomElem+".jpg";
+
+    foto.setAttribute("src", rutaImg);
+}
+
+//Funció per mostrar el camp textArea
+function mostraDescripcio(){
+    var campDesc = document.getElementById("fDescripcio");
+    campDesc.style.display = "block";
+}
+
+//APARTAT DE VALIDACIÓ DEL FORMULARI
+
