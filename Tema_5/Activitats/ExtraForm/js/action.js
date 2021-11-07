f745b6860298e5b5bfc5693db2761cf7b68e02aa
Jordi García Pons <83701435+jordi-gp@users.noreply.github.com>
2021-11-07 17:43:01 +0100

    Activitat Extra Form

---
+++ Tema_5/Activitats/ExtraForm/js/action.js
@@ -0,0 +1,9 @@
+// la imatge de la esquerra ha de canviar quna canvia el desplegable. 
+
+//s'hha de validar el número de serie per a que el numero cumplisca 
+// les seguent regles : 3 numeros inicials, 4 lletres en majúscules
+// i acabar amb el níumero 1 o 2 , o amb la lletra A
+
+//  Al clickar en mostrar Descripció apareixà el text area per poder excriure les dades.
+// cada vegada que s'escriga una paraula al text area s'ha de contar el númerode paraules.
+ 
