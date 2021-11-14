d158f8dde7b5ca927b3363b5b856d0d321156002
Jordi García Pons <83701435+jordi-gp@users.noreply.github.com>
2021-11-14 20:12:54 +0100

    esta atividade tem sido pura dor e sofrimento

--- Tema_6/TestAlumnes/js/funcions.js
+++ Tema_6/TestAlumnes/js/funcions.js
@@ -1,23 +1,75 @@
 $(document).ready(main);
     
 function main(){
-    //TODO -> mostrar les preguntes (done)
-    mostraPregunta();
+    iniciar();
 }
 
-function mostraPregunta(){
-    $("#iniciar").click(function() {
-        $("#panel").show(1500);
-        $("#pregunta").html(test[0].pregunta);
+var contador = 0;
+var acerts = 0;
+
+//Funció per iniciar el programa
+function iniciar(){
+    $("#iniciar").click(function(){
+        mostraPregunta();
         mostraRespostes();
+        $("#panel").show(1500);
     });
 }
 
+//Funció per mostrar la pregunta
+function mostraPregunta(){
+    $("#pregunta").append(test[contador].pregunta);
+}
+
+//Funció per mostrar les respostes
 function mostraRespostes(){
-    $("#iniciar").css("display", "none");
-    $("#respostes").append("<ul id='valRespostes'>");
-        for(var i=0; i < test[0].respostes.length; i++){
-            $("#valRespostes").append("<li id="+i+">"+test[0].respostes[i]+"</li>");
+    $("#postes").empty();
+    $("#pregunta").html(test[contador].pregunta);
+    $("#respostes").html("<ul id='valRespostes'>");
+        for(var i=0; i < test[contador].respostes.length; i++){
+            $("#valRespostes").append("<li id="+i+">"+test[contador].respostes[i]+"</li>");
         }
+    contador++;
+
+    canviaPregunta();
+
     $("#respostes").append("</ul>");
+}
+
+//Funció per canviar de pregunta
+function canviaPregunta(){
+    $("#valRespostes").click(function(){
+        $("#panel").hide(1500);
+
+        if(contador < 5){
+            $("#panel").show(function(){
+                mostraPregunta();
+                mostraRespostes();
+            });
+        } else {
+            $("#resultat").show(resultat());
+        }
+
+        validaPregunta();
+        //resultat();
+    });
+}
+
+//Funció per validar la pregunta correcta
+function validaPregunta(){
+    //console.log(event.target.id);
+    //console.log(test[contador-1].acert);
+    if(event.target.id == test[contador - 1].acert){
+        acerts++;
+        $("#acerts").text(acerts);
+        $("#total").text(contador);
+    } else {
+        $("#total").text(contador);
+    }
+}
+
+function resultat(){
+    var nota = acerts/5;
+    $("#resultat").append("Has acertat un total de "+acerts+" preguntes sobre "+contador+"<br>");
+    $("#resultat").append("La teva nota es de "+nota);
 }
