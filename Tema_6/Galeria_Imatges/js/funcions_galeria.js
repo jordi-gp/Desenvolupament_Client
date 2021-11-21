b6cbb3e28f97d99aaa00b16361313e27c769eca3
Jordi García Pons <83701435+jordi-gp@users.noreply.github.com>
2021-11-21 23:02:56 +0100

    a

---
+++ Tema_6/Galeria_Imatges/js/funcions_galeria.js
@@ -0,0 +1,12 @@
+$(document).ready(main);
+
+function main(){
+    $(".images").click(function(){
+        console.log("a");
+    });
+    galeria();
+}
+
+function galeria(){
+    console.log($(".images"));
+}
