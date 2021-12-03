9035ed5646a627b31139c91895ad0a2e2aee921a
Jordi García Pons <83701435+jordi-gp@users.noreply.github.com>
2021-12-03 19:04:00 +0100

    Activitat Guiada Ajax

---
+++ Tema_8/Activitat_Guiada/js/funcions_act.js
@@ -0,0 +1,16 @@
+function getVoto(int) {
+    //Creació de l'objecte XMLHttpRequest
+    xmlhttp = new XMLHttpRequest();
+
+    //Funció anònima per determinar que s'han complit
+    //els 4 estats i les respostes han sigut correcte
+    xmlhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200) {
+            document.getElementById("encuesta").innerHTML = this.responseText;
+        }
+    }
+    //Enviament de l'informació obtinguda al fitxer '.php'
+    //encarregat de processar l'informació
+    xmlhttp.open("GET", "./php/enquesta_vot.php?voto="+int, true);
+    xmlhttp.send();
+}
