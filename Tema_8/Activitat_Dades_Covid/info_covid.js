9edec81fb16c03de52869d2bca320a2d7f53c247
Jordi García Pons <83701435+jordi-gp@users.noreply.github.com>
2021-12-13 21:54:38 +0100

    Activitat Informació sobre Covid-19 update

--- Tema_8/Activitat_Dades_Covid/info_covid.js
+++ Tema_8/Activitat_Dades_Covid/info_covid.js
@@ -15,8 +15,18 @@ function cridaApi() {
             var info = JSON.parse(this.responseText);
             var dataApi = "2020-03-22";
 
-            //console.log(info.dates.dataApi.countries);
-            console.log(info.dates[dataApi].countries);
+            var totalInfec = document.getElementsByClassName("nous_infectats");
+            var totalDef = document.getElementsByClassName("noves_defuncions");
+
+            for(var i=0; i < 3; i++){
+                console.log(info.dates[dataApi].countries.Spain.regions[7].sub_regions);
+
+                var totalInfectats = info.dates[dataApi].countries.Spain.regions[7].sub_regions[i].today_confirmed;
+                var totalDefuncions = info.dates[dataApi].countries.Spain.regions[7].sub_regions[i].today_deaths;
+                
+                totalInfec[i].innerHTML = totalInfectats;
+                totalDef[i].innerHTML = totalDefuncions;
+            }
         }
     }
     xmlhttp.open("GET", "https://api.covid19tracking.narrativa.com/api/2020-03-22/country/spain");
