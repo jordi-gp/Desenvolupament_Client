695fdd5daa6a4e5ca2b2354e5b8940941ad5fddf
Jordi Garcia <jordigar00@gmail.com>
2021-12-13 13:04:00 +0100

    Activitat casos covid update

--- Tema_8/Activitat_Dades_Covid/info_covid.js
+++ Tema_8/Activitat_Dades_Covid/info_covid.js
@@ -1,16 +1,24 @@
 window.onload = main;
 
 function main(){
-
+    cridaApi();
+    
 }
 
 /*TODO:Cridar a l'api per poder obtindre l'informació*/
-function cridaApi(comunitat) {
+function cridaApi() {
+    /*Informació que anem a treure de l'arxiu JSON obtés de l'API*/
     var xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200){
             var info = JSON.parse(this.responseText);
+            var dataApi = "2020-03-22";
+
+            //console.log(info.dates.dataApi.countries);
+            console.log(info.dates[dataApi].countries);
         }
     }
+    xmlhttp.open("GET", "https://api.covid19tracking.narrativa.com/api/2020-03-22/country/spain");
+    xmlhttp.send();
 }
