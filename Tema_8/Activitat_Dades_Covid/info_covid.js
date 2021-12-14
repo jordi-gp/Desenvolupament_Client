b645c4ba142186acc20e1164a3ed0d3ed979f095
Jordi García Pons <83701435+jordi-gp@users.noreply.github.com>
2021-12-14 18:45:34 +0100

    Activitat informació sobre covid-19

--- Tema_8/Activitat_Dades_Covid/info_covid.js
+++ Tema_8/Activitat_Dades_Covid/info_covid.js
@@ -2,10 +2,12 @@ window.onload = main;
 
 function main(){
     getFecha();
-    cridaApi();
+    cridaApi(dataActual);
 }
 
 //Obtención de la fecha
+var dataActual;
+
 function getFecha(){
     var meses = ["1","2","3","4","5","6","7","8","9","10","11","12"];
     var fecha = new Date;
@@ -18,32 +20,40 @@ function getFecha(){
         dia = "0"+fecha.getDate();
     }
 
-    var dataActual = año+"-"+mes+"-"+dia;
-    console.log(dataActual);
+    dataActual = año+"-"+mes+"-"+dia;
+    return dataActual;
 }
 
 //Obtención de información de la API
-function cridaApi() {
+function cridaApi(dataActual) {
     var xmlhttp = new XMLHttpRequest();
     
     xmlhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200){
             var info = JSON.parse(this.responseText);
 
-            var totalInfec = document.getElementsByClassName("nous_infectats");
-            var totalDef = document.getElementsByClassName("noves_defuncions");
+            var numRegio = 6;
+            var infoObtinguda = info.dates[dataActual].countries.Spain.regions[numRegio].sub_regions;
+
+            var totalInfec = document.getElementsByClassName("total_infectats");
+            var totalDef = document.getElementsByClassName("total_defuncions");
             var nuevosInfec = document.getElementsByClassName("nous_infectats");
             var nuevasDef = document.getElementsByClassName("noves_defuncions");
             var ultimaAct = document.getElementsByClassName("ultima_act");
 
             for(var i=0; i < 3; i++){
-                //var totalInfectats = info.dates[dataApi].countries.Spain.regions[7].sub_regions[i].today_confirmed;
-                //var totalDefuncions = info.dates[dataApi].countries.Spain.regions[7].sub_regions[i].today_deaths;
-                
-                //totalInfec[i].innerHTML = totalInfectats;
-                //totalDef[i].innerHTML = totalDefuncions;
+                var todayConfirmed = infoObtinguda[i].today_confirmed;
+                var todayDeaths = infoObtinguda[i].today_deaths;
+                var newConfirmed = infoObtinguda[i].today_new_confirmed;
+                var newDeaths = infoObtinguda[i].today_new_deaths;
+                var lasAct = infoObtinguda[i].date;
+
+                totalInfec[i].innerHTML = todayConfirmed;
+                totalDef[i].innerHTML = todayDeaths;
+                nuevosInfec[i].innerHTML = newConfirmed;
+                nuevasDef[i].innerHTML = newDeaths;
+                ultimaAct[i].innerHTML = lasAct;
             }
-            console.log(info.dates[dataApi].countries.Spain.regions[6].sub_regions);
         }
     }
     xmlhttp.open("GET", "https://api.covid19tracking.narrativa.com/api/"+dataActual+"/country/spain");
