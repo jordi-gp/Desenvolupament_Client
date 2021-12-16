981cbae23ac08adecb080b246d98fb100c1337c4
Jordi Garcia <jordigar00@gmail.com>
2021-12-16 09:57:19 +0100

    API llibres update

--- Tema_8/Activitat_Api_Llibres/code/js/llistar_llibres.js
+++ Tema_8/Activitat_Api_Llibres/code/js/llistar_llibres.js
@@ -8,7 +8,7 @@ function main(){
 
 function cridaApi(){
     var url = "https://www.serverred.es/api/libros";
-    var llista = document.getElementById("info_llibres");
+    var llista = document.getElementById("files");
 
     fetch(url)
         .then(response => response.json())
@@ -20,9 +20,14 @@ function cridaApi(){
                 var precio = element.precio;
                 var num_mods = element.__v;
                 var id_llibre = element._id;
-                //console.log("---------------");
-                llista.innerHTML = "<th>"+titulo+"</th>";
-                console.log(titulo);
+                var autor = element.autor;
+
+                llista.innerHTML = "<th></th>"+
+                                    "<th>"+num_mods+"</th>"+
+                                    "<th>"+titulo+"</th>"+
+                                    "<th>"+editorial+"</th>"+
+                                    "<th>"+precio+"</th>"+
+                                    "<th>"+autor+"</th>";
             })
         )
 
