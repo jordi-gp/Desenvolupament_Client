c2641636e13373738841e708c8a0be2b5565266e
Jordi García Pons <83701435+jordi-gp@users.noreply.github.com>
2022-01-24 18:38:31 +0100

    a

--- Tema_8/Restaurant_Ajax/code/js/listado_comandas.js
+++ Tema_8/Restaurant_Ajax/code/js/listado_comandas.js
@@ -2,12 +2,7 @@ window.onload = main;
 
 function main() {
     compruebaLog();
-    getDatos();
-    document.getElementById("newComanda").addEventListener("click", altaComanda);
-}
-
-function altaComanda() {
-    location.assign("../html/altaComandas.html");
+    getComandas();
 }
 
 var arrComandas = [];
@@ -20,15 +15,8 @@ const apiComandas = "https://restaurante.serverred.es/api/comandas";
 const apiCamareros = "https://restaurante.serverred.es/api/camareros";
 const apiMesas = "https://restaurante.serverred.es/api/mesas";
 
-async function getDatos() {
-    await getMesas();
-    await getCamareros();
-    await getComandas();    
-}
-
-
-async function getComandas() {
-    await fetch(apiComandas, {
+function getComandas() {
+    fetch(apiComandas, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -36,81 +24,67 @@ async function getComandas() {
             }
         })
         .then(response => response.json())
-        //data => arrComandas = data.data.data
         .then(data => data.data.data.forEach(element => {
-            arrComandas.push(element);
             addComanda(element);
+            arrComandas.push(element);
         }))
+        .then(getCamareros())
         .catch((error) => {
             console.log("Error => ", error);
         });
 }
 
-async function getCamareros() {
+function getCamareros() {
     fetch(apiCamareros, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": token.token
-            }
-        })
-        .then(response => response.json())
-        .then(data => data.data.data.forEach(element => {
-            arrCamareros.push(element);
-        }))
-        .catch((error) => {
-            console.log("Error => ", error);
-        })
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "auth-token": token.token
+        }
+    })
+    .then(response => response.json())
+    .then(data => data.data.data.forEach(element => {
+        arrCamareros.push(element);
+    }))
+    .then(getMesas())
+    .catch((error) => {
+        console.log("Error => ", error);
+    })
 }
 
-async function getMesas() {
+function getMesas() {
     fetch(apiMesas, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": token.token
-            }
-        })
-        .then(response => response.json())
-        .then(data => data.data.data.forEach(element => {
-            arrMesas.push(element);
-        }))
-
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "auth-token": token.token
+        }
+    })
+    .then(response => response.json())
+    .then(data => data.data.data.forEach(element => {
+        arrMesas.push(element);
+    }))
+    .then(console.log(arrComandas), console.log(arrCamareros), console.log(arrMesas))
 }
 
 function addComanda(element) {
     //Botones para editar la comanda de bebidas
-    var logoSumBebidas = document.createElement("i");
-    logoSumBebidas.setAttribute("class", "fas fa-plus");
-    var editBebidas = document.createElement("button");
-    editBebidas.setAttribute("class", "btn btn-info btn-lg p-2");
-    var val_edit_bebidas = document.createTextNode("Bebidas");
-    editBebidas.appendChild(logoSumBebidas);
-    editBebidas.appendChild(val_edit_bebidas);
-
     //Botón para editar la comanda de platos
-    var logoSumPlatos = document.createElement("i");
-    logoSumPlatos.setAttribute("class", "fas fa-plus");
-    var editPlatos = document.createElement("button");
-    editPlatos.setAttribute("class", "btn btn-warning btn-lg m-2 p-2");
-    var val_edit_platos = document.createTextNode("Platos");
-    editPlatos.appendChild(logoSumPlatos);
-    editPlatos.appendChild(val_edit_platos);
 
     //Información obtenida del JSON
     var nombre = element.nombre;
     var mesa = element.mesa;
     var comensales = element.comensales;
     var camarero = element.user;
-    var fechaEnt = new Date(element.fechaEntrada);
+    var fechaEnt = element.fechaEntrada;
 
     //Nodos de texto utilizados
     var val_nombre = document.createTextNode(nombre);
-    var val_mesa = document.createTextNode(buscaMesa(mesa));
+    var val_mesa = document.createTextNode(buscarMesa(mesa));
     var val_comensales = document.createTextNode(comensales);
     var val_camarero = document.createTextNode(buscarCamarero(camarero));
-    var val_fecha = document.createTextNode(fechaEnt.getHours() + ":" + fechaEnt.getDate());
-    
+    var val_fecha = document.createTextNode(fechaEnt)
+
     //Elementos del HTMl
     var tr = document.createElement("tr");
     var td1 = document.createElement("td");
@@ -121,14 +95,12 @@ function addComanda(element) {
     var td6 = document.createElement("td");
     var td7 = document.createElement("td");
 
+    console.log(element.fechaEnt);
     //Añadido de la información a los campos
-    td1.appendChild(editBebidas);
-    td2.appendChild(editPlatos);
     td3.appendChild(val_nombre);
     td4.appendChild(val_mesa);
     td5.appendChild(val_comensales);
     td6.appendChild(val_camarero);
-    td7.appendChild(val_fecha);
 
     //Añadido de las columnas a la tabla
     tr.appendChild(td1);
@@ -145,22 +117,23 @@ function addComanda(element) {
 //TODO:
 //Función para encontrar el nombre del camarero
 function buscarCamarero(val_camarero) {
-    var infoCam = arrCamareros.find(item => item._id == val_camarero);
-    return infoCam.name;
+    arrCamareros.forEach(element => {
+        if(val_camarero == element._id) {
+            val_camarero = element.name;
+            return val_camarero;
+        }
+    })
 }
 
 //Función para encontrar el nombre de la mesa
-function buscaMesa(val_mesa) {
-    var infoMesa = arrMesas.find(item => item._id == val_mesa);
-    return infoMesa.numero;
+function buscarMesa(val_mesa) {
+    arrMesas.forEach(element => {
+        if(val_mesa == element._id) {
+            val_mesa =  element.numero;
+            return val_mesa;
+        }
+    })
 }
-
-//TODO: Comprovar perqué no funciona aquesta funció
-/*function buscarMesa(mesa) {
-    //console.log(val_mesa, arrMesas)
-    var infoMesa = arrMesas.find(item => item._id == mesa);
-    console.log(infoMesa);
-}*/
 
 //Función para añadir comandas
 //Llamada a la API donde se recogen las comanda
